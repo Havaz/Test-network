be1b7f90ae3c07147e9137e2602020aa28d70cfc
John <35448929+John@users.noreply.github.com>
2019-01-24 16:06:35 +0100

    xxxxx

--- Assets/moveDude.cs
+++ Assets/moveDude.cs
@@ -19,21 +19,25 @@ public class moveDude : NetworkBehaviour
     void Update()
     {
 
-        move *= 0;
-        float rotate = 0;
 
-        if (Input.GetKey(KeyCode.A))
-            move.Set(-Time.deltaTime * 10, 0, 0);
-        if (Input.GetKey(KeyCode.D))
-            move.Set(Time.deltaTime * 10, 0, 0);
-        if (Input.GetKey(KeyCode.W))
-           move.Set(0, Time.deltaTime * 10, 0);
-        if (Input.GetKey(KeyCode.S))
-            move.Set(0, -Time.deltaTime * 10, 0);
+        if (isLocalPlayer)
+        {
+            move *= 0;
+            float rotate = 0;
+
+            if (Input.GetKey(KeyCode.A))
+                move.Set(-Time.deltaTime * 10, 0, 0);
+            if (Input.GetKey(KeyCode.D))
+                move.Set(Time.deltaTime * 10, 0, 0);
+            if (Input.GetKey(KeyCode.W))
+                move.Set(0, Time.deltaTime * 10, 0);
+            if (Input.GetKey(KeyCode.S))
+                move.Set(0, -Time.deltaTime * 10, 0);
 
 
-         transform.position += move;
-         transform.Rotate (0,0,0);
+            transform.position += move;
+            transform.Rotate(0, 0, 0);
+        }
 
     }
 }
